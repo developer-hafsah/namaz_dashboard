04589ab8e9b7aa0b7cbe3602ec51cf4bd94b957e
Hafsah Siddiqah <hafsahsiddiqah@gmail.com>
2026-01-05 03:46:33 +0500

    Adding Interactive display

---
+++ script.js
@@ -0,0 +1,13 @@
+     //In your JavaScript file (script.js):
+document.getElementById('actionButton').addEventListener('click', function() {
+    window.location.href = '2_monthly_trends.html'; // Replace 'yourfile.html' with the desired HTML file
+}); 
+document.getElementById('actionButton1').addEventListener('click', function() {
+    window.location.href = '4_weekly_patterns.html'; // Replace 'yourfile.html' with the desired HTML file
+}); 
+document.getElementById('actionButton2').addEventListener('click', function() {
+    window.location.href = '5_prayer_composition.html'; // Replace 'yourfile.html' with the desired HTML file
+}); 
+document.getElementById('actionButton3').addEventListener('click', function() {
+    window.location.href = '7_interactive_dashboard.html'; // Replace 'yourfile.html' with the desired HTML file
+}); 
